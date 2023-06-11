ad6ed6c69d736f234eeeeeaca8fef7fe50fb3eb5
TLRKiliann <philogenie@protonmail.com>
2023-06-11 11:29:58 +0200

    getPosts methods setup

--- vue-project/src/components/SecondComp.vue
+++ vue-project/src/components/SecondComp.vue
@@ -1,16 +1,46 @@
+<template>
+    <h2>SecondComp</h2>
+    <div v-for="post in getPosts" :key="post?.id">
+        <div v-if="post">
+            <h2>{{ post.id }}</h2>
+            <h2>{{ post.title }}</h2>
+            <h3>{{ post.body }}</h3>
+        </div>
+        <div v-else="error">
+            <h2>{{ error?.message }}</h2>
+        </div>
+    </div>
+</template>
+
 <script lang="ts">
+import axios from "axios";
 import { defineComponent } from 'vue';
+type DataProps = {
+    id: number,
+    title: string,
+    body: string
+}
 export default defineComponent({
     name:'SecondComp',
     data() {
-        return {}
+        return {
+            getPosts,
+            posts: []
+        }
+    },
+    methods: {
+        getPosts() {
+            axios.get('http://...')
+                .then((response: { data: DataProps; }) => {
+                    response.data
+                })
+                .catch((error: { message: string; }) => {
+                    console.log(error.message)
+                })
+        }
     }
 })
 </script>
-
-<template>
-    <h2>SecondComp</h2>
-</template>
 
 <style scoped>
 
