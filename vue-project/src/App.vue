b7be290f08b1a0ba3f0d70a0e04ab176e0caa841
TLRKiliann <philogenie@protonmail.com>
2023-06-11 11:17:41 +0200

    setup

---
+++ vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {}
+  }
+})
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/components/SecondComp.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name:'SecondComp',
+    data() {
+        return {}
+    }
+})
+</script>
+
+<template>
+    <h2>SecondComp</h2>
+</template>
+
+<style scoped>
+
+</style>
