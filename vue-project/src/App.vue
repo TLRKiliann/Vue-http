594f265116696dc9479fee5b31954020772f1764
TLRKiliann <philogenie@protonmail.com>
2023-06-11 13:31:07 +0200

    new route for Articles created

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,9 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'App',
-  components: {
+  /*components: {
     HelloWorld
-  },
+  },*/
   data() {
     return {}
   }
@@ -17,11 +17,14 @@ export default defineComponent({
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld />
+      <!--HelloWorld /-->
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="nav--style">
+        <div class="div--subnav">
+          <RouterLink to="/" class="home">Home</RouterLink>
+          <RouterLink to="/articles" class="articles">Articles</RouterLink>
+          <RouterLink to="/about" class="about">About</RouterLink>
+        </div>
       </nav>
     </div>
   </header>
@@ -30,5 +33,34 @@ export default defineComponent({
 </template>
 
 <style scoped>
-
+.nav--style {
+  position: fixed;
+  top: 0px;
+  right: 0px;
+  left: 0px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  font-size: 1.4rem;
+  background: linear-gradient(160deg, skyblue, dodgerblue);
+  box-shadow: 0px 0px 4px #fff;
+}
+.div--subnav {
+  width: 50%;
+  display: flex;
+  justify-content: space-around;
+}
+.home {
+  color: cyan;
+  text-shadow: 1px 1px 1px #242424;
+}
+.articles {
+  color: cyan;
+  text-shadow: 1px 1px 1px #242424;
+}
+.about {
+  color: cyan;
+  text-shadow: 1px 1px 1px #242424;
+}
 </style>
