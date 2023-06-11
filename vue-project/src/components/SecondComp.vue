0eb5b65181018b378d976e63540bf2aad2974953
TLRKiliann <philogenie@protonmail.com>
2023-06-11 13:30:14 +0200

    replaced by a simple title

--- vue-project/src/components/SecondComp.vue
+++ vue-project/src/components/SecondComp.vue
@@ -1,52 +1,17 @@
 <template>
     <div>
         <h2>SecondComp</h2>
-        <button @click="getPosts">Loader</button>
-        <div v-if="errorMsg">
-            <h2>{{ errorMsg }}</h2>
-        </div>
-        <div v-for="post in posts.slice(0, 20)" :key="post.id">
-            <h2>{{ post.id }}</h2>
-            <h2>{{ post.title }}</h2>
-            <h3>{{ post.body }}</h3>
-            <hr />
-        </div>
     </div>
 </template>
 
 <script lang="ts">
-import axios, { type AxiosResponse } from "axios";
 import { defineComponent } from 'vue';
-type DataPosts = {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-}
 export default defineComponent({
     name:'SecondComp',
-    /*created() {
-        this.getPosts()
-    },*/
     data() {
         return {
-            posts: [],
-            errorMsg: '',
         }
     },
-    methods: {
-        getPosts() {
-            axios.get<DataPosts[]>('https://jsonplaceholder.typicode.com/posts')
-                .then((response: AxiosResponse) => {
-                    console.log(response.data)
-                    this.posts = response.data
-                })
-                .catch((error: string) => {
-                    console.log(error)
-                    this.errorMsg = error
-                })
-        }
-    }
 })
 </script>
 
